5e1d1f40c6fa9bcac18cc146fb0b46c985c96955
Ajaydama <damaajay222@gmail.com>
2024-04-01 12:23:34 +0000

    Initial commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Start Nginx
+service nginx start
+
+# cd cbi-hospital-embedded
+
+# pip install -r requirements.txt
+# # Run Flask application
+# flask run --host=0.0.0.0 --reload
+
+sleep infinity
